750b541c32c6fa7620fc99c1e7202d715a7376a4
Debian <admin@apex.sccwrp.org>
2022-10-27 18:23:43 +0000

    added template download link for intercal checker

--- proj/download.py
+++ proj/download.py
@@ -1,6 +1,11 @@
 import os
 from flask import send_file, Blueprint, jsonify, request, g, current_app
 from pandas import read_sql
+from io import BytesIO
+
+from shareplum import Site
+from shareplum import Office365
+from shareplum.site import Version
 
 download = Blueprint('download', __name__)
 @download.route('/download/<submissionid>/<filename>', methods = ['GET','POST'])
@@ -37,3 +42,28 @@ def template_file():
 
     else:
         return jsonify(message = "neither a filename nor a database tablename were provided")
+
+
+@download.route('/template')
+def get_template():
+    
+    username = os.environ.get('MS_USERNAME')
+    password = os.environ.get('MS_PASSWORD')
+    url = os.environ.get('SHAREPOINT_SITE_URL')
+    teamname = os.environ.get('TEAMS_SITE_NAME')
+    filename = os.environ.get('TEMPLATE_FILE_NAME')
+    sitefolder = os.environ.get('SUBMISSION_TEMPLATE_SITE_FOLDER')
+
+    authcookie = Office365(url, username=username, password=password).GetCookies()
+    site = Site(os.path.join(url, 'sites', teamname), version=Version.v2016, authcookie=authcookie)
+    print(os.path.join(url, 'sites', teamname))
+    print(sitefolder)
+    folder = site.Folder(sitefolder)
+    # 
+    for f in folder.files:
+        if f.get('Name') == filename:
+            data = BytesIO(folder.get_file(filename))
+            return send_file(data, as_attachment = True, download_name = filename, mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        
+    return 'Not found'
+        
