88728a0e244648b81d335cf677cb5d34ab2cb820
r7butler <40872002+r7butler@users.noreply.github.com>
2022-11-17 11:41:08 -0800

    if it did not find right digits in a float column, the variable called right should be set to zero

--- proj/core/functions.py
+++ proj/core/functions.py
@@ -105,8 +105,9 @@ def check_precision(x, precision):
         
         if rightdigits: # if its not a NoneType, it found a match
             rightdigits = rightdigits.groups()[0]
-        
-        right = powerof10 + len(rightdigits)
+            right = powerof10 + len(rightdigits)
+        else:
+            right = 0
     else:
         # frac part is zero if there is no decimal place, or if it came in with scientific notation
         # because this else block represents the case where the power was positive
